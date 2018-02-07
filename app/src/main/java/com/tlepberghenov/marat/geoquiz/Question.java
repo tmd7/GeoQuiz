e3ff0b87fef9f2c87362227e7bf4a346b22e2ed5
Marat Tlepberghenov <mur.marat@gmail.com>
2018-02-08 01:50:01 +0200

    Added class Question.
    Signed-off-by: Marat Tlepberghenov <mur.marat@gmail.com>

---
+++ app/src/main/java/com/tlepberghenov/marat/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.tlepberghenov.marat.geoquiz;
+
+/**
+ * Created by Marat on 08.02.2018.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+
+}

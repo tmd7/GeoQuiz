6f36db2fcedce732817d5a00170f67a4254b8e80
Marat Tlepberghenov <mur.marat@gmail.com>
2018-02-08 00:35:13 +0200

    Connect widgets and program.
    Signed-off-by: Marat Tlepberghenov <mur.marat@gmail.com>

--- app/src/main/java/com/tlepberghenov/marat/geoquiz/QuizActivity.java
+++ app/src/main/java/com/tlepberghenov/marat/geoquiz/QuizActivity.java
@@ -2,12 +2,34 @@ package com.tlepberghenov.marat.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }

dba1fb56735978b3da795ad1be2972cf7bec2272
Marat Tlepberghenov <mur.marat@gmail.com>
2018-03-02 00:03:30 +0200

    Added Intent in cheat button.
    Signed-off-by: Marat Tlepberghenov <mur.marat@gmail.com>

--- app/src/main/java/com/tlepberghenov/marat/geoquiz/QuizActivity.java
+++ app/src/main/java/com/tlepberghenov/marat/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.tlepberghenov.marat.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -112,6 +113,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Start CheatActivity
+                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(intent);
             }
         });
 

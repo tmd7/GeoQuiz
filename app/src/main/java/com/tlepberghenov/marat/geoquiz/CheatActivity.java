83c933514fe16ba09d02581d4f2876e630380a8a
Marat Tlepberghenov <mur.marat@gmail.com>
2018-03-02 00:45:17 +0200

    Added function for overview tru answer.
    Signed-off-by: Marat Tlepberghenov <mur.marat@gmail.com>

--- app/src/main/java/com/tlepberghenov/marat/geoquiz/CheatActivity.java
+++ app/src/main/java/com/tlepberghenov/marat/geoquiz/CheatActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_IS_TRUE = "com.tlepberghenov.marat.geoquiz.answer_is_true";
     private boolean mAnswerIsTrue;
+
+    private TextView mAnserTextVieaw;
+    private Button mShowAnswerButton;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -20,5 +26,18 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnserTextVieaw = (TextView) findViewById(R.id.answer_text_view);
+        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnserTextVieaw.setText(R.string.true_button);
+                } else {
+                    mAnserTextVieaw.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }

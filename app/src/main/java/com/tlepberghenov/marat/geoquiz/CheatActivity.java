e00071f534f2b7179c34962a92a4ad3b83f1ba2e
Marat Tlepberghenov <mur.marat@gmail.com>
2018-03-02 00:23:52 +0200

    Added static method newIntent for extra data.
    Signed-off-by: Marat Tlepberghenov <mur.marat@gmail.com>

--- app/src/main/java/com/tlepberghenov/marat/geoquiz/CheatActivity.java
+++ app/src/main/java/com/tlepberghenov/marat/geoquiz/CheatActivity.java
@@ -1,10 +1,18 @@
 package com.tlepberghenov.marat.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_IS_TRUE = "com.tlepberghenov.marat.geoquiz.answer_is_true";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

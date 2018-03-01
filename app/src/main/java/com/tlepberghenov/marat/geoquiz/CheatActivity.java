87172580275dc7f35ab68ec642fed723b573d6c1
Marat Tlepberghenov <mur.marat@gmail.com>
2018-03-02 00:17:09 +0200

    Added constant in cheat activity.
    Signed-off-by: Marat Tlepberghenov <mur.marat@gmail.com>

--- app/src/main/java/com/tlepberghenov/marat/geoquiz/CheatActivity.java
+++ app/src/main/java/com/tlepberghenov/marat/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.tlepberghenov.marat.geoquiz.answer_is_true";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
